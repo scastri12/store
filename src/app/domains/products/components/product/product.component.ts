f7065cd9bf33eb1f264fe45ccba1d965da0173ba
Santiago Castrillon <scastrillon@loyaltysci.com>
2024-06-14 21:04:38 -0500

    ci: layout product with inputs and outputs

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule} from '@angular/common'
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
-  img = 'https://picsum.photos/640/640?r=' + Math.random();
+  @Input({required: true}) img: string = '';
+  @Input({required: true}) price: number = 0;
+  @Input({required: true}) title: string = '';
+
+  @Output() addToCart = new EventEmitter();
+
+  addToCartHandler() {
+    console.log('click form child');
+    this.addToCart.emit('Hola este es un msg desde el hijo ' + this.title);
+  }
+
+
 
 }
