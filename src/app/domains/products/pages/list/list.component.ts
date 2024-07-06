1cc02a59833acd3062db4c263108b44fd8c99d4f
Santiago Castrillon <scastrillon@loyaltysci.com>
2024-07-05 21:10:55 -0500

    ci: add and implement product service

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -4,6 +4,7 @@ import { ProductComponent } from './../../components/product/product.component';
 import { Product } from './../../../shared/models/product.model'
 import { HeaderComponent } from './../../../shared/components/header/header.component'
 import { CartService } from '../../../shared/services/cart.service';
+import { ProductService } from '../../../shared/services/product.service';
 
 @Component({
   selector: 'app-list',
@@ -16,56 +17,19 @@ export class ListComponent {
 
   products = signal<Product[]>([]);
   private cartService = inject(CartService);
+  private productService = inject(ProductService);
 
-
-  constructor() {
-    const initProducts: Product[] = [
-      {
-        id: Date.now(),
-        title: 'Pro 1',
-        price: 100,
-        image: "http://picsum.photos/640/640?r=23",
-        creationAt: new Date().toISOString()
+  ngOnInit() {
+    this.productService.getProducts()
+    .subscribe({
+      next: (products) => {
+        this.products.set(products);
       },
-      {
-        id: Date.now(),
-        title: 'Pro 2',
-        price: 100,
-        image: "http://picsum.photos/640/640?r=13",
-        creationAt: new Date().toISOString()
-      },
-      {
-        id: Date.now(),
-        title: 'Pro 3',
-        price: 100,
-        image: "http://picsum.photos/640/640?r=143",
-        creationAt: new Date().toISOString()
-      },
-      {
-        id: Date.now(),
-        title: 'Pro 1',
-        price: 100,
-        image: "http://picsum.photos/640/640?r=23",
-        creationAt: new Date().toISOString()
-      },
-      {
-        id: Date.now(),
-        title: 'Pro 2',
-        price: 100,
-        image: "http://picsum.photos/640/640?r=13",
-        creationAt: new Date().toISOString()
-      },
-      {
-        id: Date.now(),
-        title: 'Pro 3',
-        price: 100,
-        image: "http://picsum.photos/640/640?r=143",
-        creationAt: new Date().toISOString()
+      error: () => {
+        
       }
-    ];
-    this.products.set(initProducts);
+    })
   }
-
   addToCart(product: Product) {
     this.cartService.addToCart(product);
 
