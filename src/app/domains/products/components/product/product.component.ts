dd054d4940cc834ec2f8b85063d4df2ca5977f38
Santiago Castrillon <scastrillon@loyaltysci.com>
2024-07-07 18:13:53 -0500

    ci: implement product detail with service

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { CommonModule} from '@angular/common'
+import { CommonModule} from '@angular/common';
 import { Product } from '@shared/models/product.model';
 import { ReversePipe } from '@shared/pipes/reverse.pipe';
 import { TimeAgoPipe } from '@shared/pipes/time-ago.pipe';
--- src/app/domains/products/pages/product-detail/product-detail.component.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input, signal } from '@angular/core';
+import { CommonModule} from '@angular/common';
+import { Product } from '@shared/models/product.model';
+import { ProductService } from '@shared/services/product.service';
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
 })
 export class ProductDetailComponent {
+
+  @Input() id?: string;
+  product = signal<Product | null>(null);
+  cover = signal('');
+  private productService = inject(ProductService);
+
+  ngOnInit() {
+    if(this.id) {
+      this.productService.getOne(this.id)
+      .subscribe({
+        next: (product) => {
+          this.product.set(product);
+          if (product.images.length > 0) {
+            this.cover.set(product.images[0]);
+            
+          }
+        }
+      });
+    };
+  }
+
+  changeCover(newImg: string) {
+    this.cover.set(newImg);
+  }
 
 }
