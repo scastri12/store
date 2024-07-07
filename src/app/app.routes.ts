5bc4bb89cbca5c44910b16134ba54f84e3f6dcb6
Santiago Castrillon <scastrillon@loyaltysci.com>
2024-07-07 13:57:30 -0500

    ci: implement anided views

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,16 +2,23 @@ import { Routes } from '@angular/router';
 import { ListComponent } from './domains/products/pages/list/list.component';
 import { AboutComponent } from '@info/pages/about/about.component'; 
 import { NotFoundComponent } from '@info/pages/not-found/not-found.component'; 
+import { LayoutComponent } from '@shared/components/layout/layout.component';
 
 
 export const routes: Routes = [
     {
         path: '',
-        component: ListComponent
-    },
-    {
-        path: 'about',
-        component: AboutComponent
+        component: LayoutComponent,
+        children: [
+            {
+                path: '',
+                component: ListComponent
+            },
+            {
+                path: 'about',
+                component: AboutComponent
+            },
+        ]
     },
     {
         path: '**',
