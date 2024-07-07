a6c9a07abbafc394e75db8681cd72e2545d8adbf
Santiago Castrillon <scastrillon@loyaltysci.com>
2024-07-07 14:21:33 -0500

    ci: add product detail component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { ListComponent } from './domains/products/pages/list/list.component';
+import { ProductDetailComponent } from '@products/pages/product-detail/product-detail.component';
 import { AboutComponent } from '@info/pages/about/about.component'; 
 import { NotFoundComponent } from '@info/pages/not-found/not-found.component'; 
 import { LayoutComponent } from '@shared/components/layout/layout.component';
@@ -18,6 +19,10 @@ export const routes: Routes = [
                 path: 'about',
                 component: AboutComponent
             },
+            {
+                path: 'product/:id',
+                component: ProductDetailComponent
+            }
         ]
     },
     {
--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -3,13 +3,14 @@ import { CommonModule} from '@angular/common'
 import { Product } from '@shared/models/product.model';
 import { ReversePipe } from '@shared/pipes/reverse.pipe';
 import { TimeAgoPipe } from '@shared/pipes/time-ago.pipe';
+import { RouterLink } from '@angular/router';
 
 
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [CommonModule, ReversePipe, TimeAgoPipe],
+  imports: [CommonModule, ReversePipe, TimeAgoPipe, RouterLink],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
