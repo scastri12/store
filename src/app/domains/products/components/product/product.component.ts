7d1c0e0d527aa84a80d59ca9083154b16f6f3572
Santiago Castrillon <scastrillon@loyaltysci.com>
2024-07-07 12:48:02 -0500

    ci: add custom pipes time ago and reverse

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,10 +1,15 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule} from '@angular/common'
 import { Product } from '@shared/models/product.model';
+import { ReversePipe } from '@shared/pipes/reverse.pipe';
+import { TimeAgoPipe } from '@shared/pipes/time-ago.pipe';
+
+
+
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReversePipe, TimeAgoPipe],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
